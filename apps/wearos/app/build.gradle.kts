4f0b5e90269d8ead48c57691226a42f68bc51823
Mahendra Bishnoi <mahendrabishnoi2@gmail.com>
2026-07-16 16:52:37 +0530

    chore: prepare release 0.1.3

--- apps/wearos/app/build.gradle.kts
+++ apps/wearos/app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         // listing. Scheme: [targetSdk:2][formFactor:1][version:6], where
         // version is major/minor/patch each zero-padded to 2 digits
         // (0.1.0 -> 000100). formFactor 1=mobile, 2=wear.
-        versionCode = 362000102
-        versionName = "0.1.2"
+        versionCode = 362000103
+        versionName = "0.1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
